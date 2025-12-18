2a35025e4aa419ba0eaf8aab974b9be7cb65166a
limsso128 <s2412@e-mirim.hs.kr>
2025-12-19 01:10:00 +0900

    상세페이지 생성

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // 책 아이템 모두 선택
     const bookItems = document.querySelectorAll('.book-item');
 
     bookItems.forEach(item => {
         item.addEventListener('click', () => {
-            const title = item.querySelector('h3').innerText;
+            const bookId = item.dataset.id; // data-id 값
+            const title = item.dataset.title; // data-title 값
             alert(`'${title}' 책 상세 페이지로 이동합니다.`);
-            // 실제로는 location.href = '상세페이지URL'; 와 같이 이동 처리를 합니다.
+            // 상세 페이지 이동
+            location.href = `detail.html?id=${bookId}`;
         });
     });
 });
